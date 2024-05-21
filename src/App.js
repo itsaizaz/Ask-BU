fa1d29318cfb6f3ce66e81ac2a5b928e914306a9
Muhammad Aizaz <muhammadaizaz650@outlook.com>
2024-05-21 14:59:28 +0500

    domain settle

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
-import React, { Suspense, useEffect, useRef, useState, useMemo } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { useGLTF, useTexture, Loader, Environment, useFBX, useAnimations, OrthographicCamera } from '@react-three/drei';
-import { MeshStandardMaterial } from 'three/src/materials/MeshStandardMaterial';
+import React, { Suspense, useEffect, useRef, useState, useMemo } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  useGLTF,
+  useTexture,
+  Loader,
+  Environment,
+  useFBX,
+  useAnimations,
+  OrthographicCamera,
+} from "@react-three/drei";
+import { MeshStandardMaterial } from "three/src/materials/MeshStandardMaterial";
 
-import { LinearEncoding, sRGBEncoding } from 'three/src/constants';
-import { LineBasicMaterial, MeshPhysicalMaterial, Vector2 } from 'three';
-import ReactAudioPlayer from 'react-audio-player';
+import { LinearEncoding, sRGBEncoding } from "three/src/constants";
+import { LineBasicMaterial, MeshPhysicalMaterial, Vector2 } from "three";
+import ReactAudioPlayer from "react-audio-player";
 
-import createAnimation from './converter';
-import blinkData from './blendDataBlink.json';
+import createAnimation from "./converter";
+import blinkData from "./blendDataBlink.json";
 
-import * as THREE from 'three';
-import axios from 'axios';
-const _ = require('lodash');
+import * as THREE from "three";
+import axios from "axios";
+const _ = require("lodash");
 
-const host = 'http://localhost:5000'
+const host = "https://ask-bu.vercel.app";  // Replace with your actual backend URL
 
-function Avatar({ avatar_url, speak, setSpeak, text, setAudioSource, playing }) {
 
+function Avatar({
+  avatar_url,
+  speak,
+  setSpeak,
+  text,
+  setAudioSource,
+  playing,
+}) {
   let gltf = useGLTF(avatar_url);
   let morphTargetDictionaryBody = null;
   let morphTargetDictionaryLowerTeeth = null;
 
-  const [ 
-    bodyTexture, 
-    eyesTexture, 
-    teethTexture, 
-    bodySpecularTexture, 
-    bodyRoughnessTexture, 
+  const [
+    bodyTexture,
+    eyesTexture,
+    teethTexture,
+    bodySpecularTexture,
+    bodyRoughnessTexture,
     bodyNormalTexture,
     teethNormalTexture,
     // teethSpecularTexture,
@@ -38,7 +53,7 @@ function Avatar({ avatar_url, speak, setSpeak, text, setAudioSource, playing }) 
     hairAlphaTexture,
     hairNormalTexture,
     hairRoughnessTexture,
-    ] = useTexture([
+  ] = useTexture([
     "/images/body.webp",
     "/images/eyes.webp",
     "/images/teeth_diffuse.webp",
@@ -56,43 +71,44 @@ function Avatar({ avatar_url, speak, setSpeak, text, setAudioSource, playing }) 
     "/images/h_roughness.webp",
   ]);
 
-  _.each([
-    bodyTexture, 
-    eyesTexture, 
-    teethTexture, 
-    teethNormalTexture, 
-    bodySpecularTexture, 
-    bodyRoughnessTexture, 
-    bodyNormalTexture, 
-    tshirtDiffuseTexture, 
-    tshirtNormalTexture, 
-    tshirtRoughnessTexture,
-    hairAlphaTexture,
-    hairNormalTexture,
-    hairRoughnessTexture
-  ], t => {
-    t.encoding = sRGBEncoding;
-    t.flipY = false;
-  });
+  _.each(
+    [
+      bodyTexture,
+      eyesTexture,
+      teethTexture,
+      teethNormalTexture,
+      bodySpecularTexture,
+      bodyRoughnessTexture,
+      bodyNormalTexture,
+      tshirtDiffuseTexture,
+      tshirtNormalTexture,
+      tshirtRoughnessTexture,
+      hairAlphaTexture,
+      hairNormalTexture,
+      hairRoughnessTexture,
+    ],
+    (t) => {
+      t.encoding = sRGBEncoding;
+      t.flipY = false;
+    }
+  );
 
   bodyNormalTexture.encoding = LinearEncoding;
   tshirtNormalTexture.encoding = LinearEncoding;
   teethNormalTexture.encoding = LinearEncoding;
   hairNormalTexture.encoding = LinearEncoding;
 
-  
-  gltf.scene.traverse(node => {
-
-
-    if(node.type === 'Mesh' || node.type === 'LineSegments' || node.type === 'SkinnedMesh') {
-
+  gltf.scene.traverse((node) => {
+    if (
+      node.type === "Mesh" ||
+      node.type === "LineSegments" ||
+      node.type === "SkinnedMesh"
+    ) {
       node.castShadow = true;
       node.receiveShadow = true;
       node.frustumCulled = false;
 
-    
       if (node.name.includes("Body")) {
-
         node.castShadow = true;
         node.receiveShadow = true;
 
@@ -110,7 +126,6 @@ function Avatar({ avatar_url, speak, setSpeak, text, setAudioSource, playing }) 
 
         node.material.envMapIntensity = 0.8;
         // node.material.visible = false;
-
       }
 
       if (node.name.includes("Eyes")) {
@@ -119,12 +134,10 @@ function Avatar({ avatar_url, speak, setSpeak, text, setAudioSource, playing }) 
         // node.material.shininess = 100;
         node.material.roughness = 0.1;
         node.material.envMapIntensity = 0.5;
-
-
       }
 
       if (node.name.includes("Brows")) {
-        node.material = new LineBasicMaterial({color: 0x000000});
+        node.material = new LineBasicMaterial({ color: 0x000000 });
         node.material.linewidth = 1;
         node.material.opacity = 0.5;
         node.material.transparent = true;
@@ -132,7 +145,6 @@ function Avatar({ avatar_url, speak, setSpeak, text, setAudioSource, playing }) 
       }
 
       if (node.name.includes("Teeth")) {
-
         node.receiveShadow = true;
         node.castShadow = true;
         node.material = new MeshStandardMaterial();
@@ -141,8 +153,6 @@ function Avatar({ avatar_url, speak, setSpeak, text, setAudioSource, playing }) 
         node.material.normalMap = teethNormalTexture;
 
         node.material.envMapIntensity = 0.7;
-
-
       }
 
       if (node.name.includes("Hair")) {
@@ -151,15 +161,13 @@ function Avatar({ avatar_url, speak, setSpeak, text, setAudioSource, playing }) 
         node.material.alphaMap = hairAlphaTexture;
         node.material.normalMap = hairNormalTexture;
         node.material.roughnessMap = hairRoughnessTexture;
-        
+
         node.material.transparent = true;
         node.material.depthWrite = false;
         node.material.side = 2;
         node.material.color.setHex(0x000000);
-        
-        node.material.envMapIntensity = 0.3;
 
-      
+        node.material.envMapIntensity = 0.3;
       }
 
       if (node.name.includes("TSHIRT")) {
@@ -171,58 +179,56 @@ function Avatar({ avatar_url, speak, setSpeak, text, setAudioSource, playing }) 
         node.material.color.setHex(0xffffff);
 
         node.material.envMapIntensity = 0.5;
-
-
       }
 
       if (node.name.includes("TeethLower")) {
         morphTargetDictionaryLowerTeeth = node.morphTargetDictionary;
       }
-
     }
-
   });
 
   const [clips, setClips] = useState([]);
   const mixer = useMemo(() => new THREE.AnimationMixer(gltf.scene), []);
 
   useEffect(() => {
-
-    if (speak === false)
-      return;
+    if (speak === false) return;
 
     makeSpeech(text)
-    .then( response => {
+      .then((response) => {
+        let { blendData, filename } = response.data;
 
-      let {blendData, filename}= response.data;
+        let newClips = [
+          createAnimation(blendData, morphTargetDictionaryBody, "HG_Body"),
+          createAnimation(
+            blendData,
+            morphTargetDictionaryLowerTeeth,
+            "HG_TeethLower"
+          ),
+        ];
 
-      let newClips = [ 
-        createAnimation(blendData, morphTargetDictionaryBody, 'HG_Body'), 
-        createAnimation(blendData, morphTargetDictionaryLowerTeeth, 'HG_TeethLower') ];
+        filename = host + filename;
 
-      filename = host + filename;
-        
-      setClips(newClips);
-      setAudioSource(filename);
-
-    })
-    .catch(err => {
-      console.error(err);
-      setSpeak(false);
-
-    })
-
+        setClips(newClips);
+        setAudioSource(filename);
+      })
+      .catch((err) => {
+        console.error(err);
+        setSpeak(false);
+      });
   }, [speak]);
 
-  let idleFbx = useFBX('/idle.fbx');
+  let idleFbx = useFBX("/idle.fbx");
   let { clips: idleClips } = useAnimations(idleFbx.animations);
 
-  idleClips[0].tracks = _.filter(idleClips[0].tracks, track => {
-    return track.name.includes("Head") || track.name.includes("Neck") || track.name.includes("Spine2");
+  idleClips[0].tracks = _.filter(idleClips[0].tracks, (track) => {
+    return (
+      track.name.includes("Head") ||
+      track.name.includes("Neck") ||
+      track.name.includes("Spine2")
+    );
   });
 
-  idleClips[0].tracks = _.map(idleClips[0].tracks, track => {
-
+  idleClips[0].tracks = _.map(idleClips[0].tracks, (track) => {
     if (track.name.includes("Head")) {
       track.name = "head.quaternion";
     }
@@ -236,41 +242,35 @@ function Avatar({ avatar_url, speak, setSpeak, text, setAudioSource, playing }) 
     }
 
     return track;
-
   });
 
   useEffect(() => {
-
     let idleClipAction = mixer.clipAction(idleClips[0]);
     idleClipAction.play();
 
-    let blinkClip = createAnimation(blinkData, morphTargetDictionaryBody, 'HG_Body');
+    let blinkClip = createAnimation(
+      blinkData,
+      morphTargetDictionaryBody,
+      "HG_Body"
+    );
     let blinkAction = mixer.clipAction(blinkClip);
     blinkAction.play();
-
-
   }, []);
 
   // Play animation clips when available
   useEffect(() => {
+    if (playing === false) return;
 
-    if (playing === false)
-      return;
-    
-    _.each(clips, clip => {
-        let clipAction = mixer.clipAction(clip);
-        clipAction.setLoop(THREE.LoopOnce);
-        clipAction.play();
-
+    _.each(clips, (clip) => {
+      let clipAction = mixer.clipAction(clip);
+      clipAction.setLoop(THREE.LoopOnce);
+      clipAction.play();
     });
-
   }, [playing]);
 
-  
   useFrame((state, delta) => {
     mixer.update(delta);
   });
-
 
   return (
     <group name="avatar">
@@ -279,21 +279,34 @@ function Avatar({ avatar_url, speak, setSpeak, text, setAudioSource, playing }) 
   );
 }
 
-
 function makeSpeech(text) {
-  return axios.post(host + '/talk', { text });
+  return axios.post(host + "/talk", { text });
 }
 
 const STYLES = {
-  area: {position: 'absolute', bottom:'10px', left: '10px', zIndex: 500},
-  text: {margin: '0px', width:'300px', padding: '5px', background: 'none', color: '#ffffff', fontSize: '1.2em', border: 'none'},
-  speak: {padding: '10px', marginTop: '5px', display: 'block', color: '#FFFFFF', background: '#222222', border: 'None'},
-  area2: {position: 'absolute', top:'5px', right: '15px', zIndex: 500},
-  label: {color: '#777777', fontSize:'0.8em'}
-}
+  area: { position: "absolute", bottom: "10px", left: "10px", zIndex: 500 },
+  text: {
+    margin: "0px",
+    width: "300px",
+    padding: "5px",
+    background: "none",
+    color: "#ffffff",
+    fontSize: "1.2em",
+    border: "none",
+  },
+  speak: {
+    padding: "10px",
+    marginTop: "5px",
+    display: "block",
+    color: "#FFFFFF",
+    background: "#222222",
+    border: "None",
+  },
+  area2: { position: "absolute", top: "5px", right: "15px", zIndex: 500 },
+  label: { color: "#777777", fontSize: "0.8em" },
+};
 
 function App() {
-
   const audioPlayer = useRef();
 
   const [speak, setSpeak] = useState(false);
@@ -312,11 +325,8 @@ function App() {
   function playerReady(e) {
     audioPlayer.current.audioEl.current.play();
     setPlaying(true);
-
-  }  
+  }
   return (
-
-    
     <div className="full">
       {/* <div style={STYLES.area}>
 
@@ -325,100 +335,89 @@ function App() {
         <button onClick={() => setSpeak(true)} style={STYLES.speak}> { speak? 'Sending...': 'Send' }</button>
 
       </div> */}
-<div style={STYLES.full}>
-  <div style={STYLES.area}>
-    <input
-      rows={4}
-      type="text"
-      style={STYLES.text}
-      value={text}
-      onChange={(e) => setText(e.target.value.substring(0, 200))}
-      placeholder='Ask me a question'
-    />
-    <button onClick={() => setSpeak(true)} style={STYLES.speak}>
-      {speak ? 'Sending...' : 'Send'}
-    </button>
-  </div>
-</div>
+      <div style={STYLES.full}>
+        <div style={STYLES.area}>
+          <input
+            rows={4}
+            type="text"
+            style={STYLES.text}
+            value={text}
+            onChange={(e) => setText(e.target.value.substring(0, 200))}
+            placeholder="Ask me a question"
+          />
+          <button onClick={() => setSpeak(true)} style={STYLES.speak}>
+            {speak ? "Sending..." : "Send"}
+          </button>
+        </div>
+      </div>
 
       <ReactAudioPlayer
         src={audioSource}
         ref={audioPlayer}
         onEnded={playerEnded}
         onCanPlayThrough={playerReady}
-        
       />
-      
+
       {/* <Stats /> */}
-    <Canvas dpr={2} onCreated={(ctx) => {
-        ctx.gl.physicallyCorrectLights = true;
-      }}>
+      <Canvas
+        dpr={2}
+        onCreated={(ctx) => {
+          ctx.gl.physicallyCorrectLights = true;
+        }}
+      >
+        <OrthographicCamera makeDefault zoom={2000} position={[0, 1.65, 1]} />
 
-      <OrthographicCamera 
-      makeDefault
-      zoom={2000}
-      position={[0, 1.65, 1]}
-      />
-
-      {/* <OrbitControls
+        {/* <OrbitControls
         target={[0, 1.65, 0]}
       /> */}
 
-      <Suspense fallback={null}>
-        <Environment background={false} files="/images/photo_studio_loft_hall_1k.hdr" />
-      </Suspense>
+        <Suspense fallback={null}>
+          <Environment
+            background={false}
+            files="/images/photo_studio_loft_hall_1k.hdr"
+          />
+        </Suspense>
 
-      <Suspense fallback={null}>
-        <Bg />
-      </Suspense>
+        <Suspense fallback={null}>
+          <Bg />
+        </Suspense>
 
-      <Suspense fallback={null}>
-
-
-
-          <Avatar 
-            avatar_url="/model.glb" 
-            speak={speak} 
+        <Suspense fallback={null}>
+          <Avatar
+            avatar_url="/model.glb"
+            speak={speak}
             setSpeak={setSpeak}
             text={text}
             setAudioSource={setAudioSource}
             playing={playing}
-            />
+          />
+        </Suspense>
+      </Canvas>
+      <Loader dataInterpolation={(p) => `Loading... please wait`} />
+    </div>
+    //   <div className="App">
+    //   <div className="chat-container">
+    //     <div className="chat">
+    //       {chat.map((message, index) => (
+    //         <div key={index} className={`message ${message.type}`}>
+    //           {message.text}
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </div>
 
-      
-      </Suspense>
-
-  
-
-  </Canvas>
-  <Loader dataInterpolation={(p) => `Loading... please wait`}  />
-  </div>
-//   <div className="App">
-//   <div className="chat-container">
-//     <div className="chat">
-//       {chat.map((message, index) => (
-//         <div key={index} className={`message ${message.type}`}>
-//           {message.text}
-//         </div>
-//       ))}
-//     </div>
-//   </div>
- 
-// </div>
-  )
+    // </div>
+  );
 }
 function Bg() {
-  
-  const texture = useTexture('/images/bg.webp');
+  const texture = useTexture("/images/bg.webp");
 
-  return(
+  return (
     <mesh position={[0, 1.5, -2]} scale={[0.8, 0.8, 0.8]}>
       <planeBufferGeometry />
       <meshBasicMaterial map={texture} />
-
     </mesh>
-  )
-
+  );
 }
 
 export default App;
